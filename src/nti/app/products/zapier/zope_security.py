5faacc58120a7bc59d347ac526b6be153bfb8560
Bobby Hagen <bobby.hagen@nextthought.com>
2020-12-08 09:44:44 -0600

    NTI-9924: Permit user data to site admins only for users they can admin.

--- src/nti/app/products/zapier/zope_security.py
+++ src/nti/app/products/zapier/zope_security.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from nti.dataserver.interfaces import ISiteAdminUtility
 from zope import component
 from zope import interface
 
@@ -32,36 +33,45 @@ class UserPrincipalPermissionMap(object):
         self.context = context
 
     @Lazy
-    def _adminsForCurrentSite(self):
-        return [user.username for user in get_site_admins()]
+    def siteAdminUtility(self):
+        return component.getUtility(ISiteAdminUtility)
+
+    def _can_admin(self, site_admin):
+        return self.siteAdminUtility.can_administer_user(site_admin,
+                                                         self.context)
+
+    @Lazy
+    def _effectiveAdminsForUser(self):
+        return [site_admin.username for site_admin in get_site_admins()
+                if self._can_admin(site_admin)]
 
     def getPrincipalsForPermission(self, perm):
         # Ensure our site admins have access to users in their site
         # e.g. for checking user object access when sending webhooks created
         # by the site admin
         result = []
-        if perm in self.SITE_ADMIN_PERMS:
-            for principal_id in self._adminsForCurrentSite:
-                # TODO: Do we need to check that the principal can admin
-                #   the user in self.context?
+        if perm in self.SITE_ADMIN_PERM_IDS:
+            for principal_id in self._effectiveAdminsForUser:
                 result.append((principal_id, Allow))
         return result
 
     def getPermissionsForPrincipal(self, principal_id):
-        if principal_id in self._adminsForCurrentSite:
+        if principal_id in self._effectiveAdminsForUser:
             return [(perm, Allow) for perm in self.SITE_ADMIN_PERM_IDS]
 
         return []
 
     def getSetting(self, permission_id, principal_id, default=Unset):
         if permission_id in self.SITE_ADMIN_PERM_IDS:
-            if principal_id in self._adminsForCurrentSite:
+            if principal_id in self._effectiveAdminsForUser:
                 return Allow
 
         return default
 
     def getPrincipalsAndPermissions(self):
         result = []
-        for principal_id in self._adminsForCurrentSite:
+        for principal_id in self._effectiveAdminsForUser:
             for perm in self.SITE_ADMIN_PERM_IDS:
-                result.append((perm, principal_id, Allow))
+                result.append((principal_id, perm, Allow))
+
+        return result
