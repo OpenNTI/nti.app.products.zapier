53522f2c51ec0f192cebfb544917e4f491456d5d
Bobby Hagen <hagenrd@users.noreply.github.com>
2020-12-09 10:00:54 -0600

    Fix import for i18nmessageid
    Co-authored-by: Jason Madden <jamadden@gmail.com>

--- src/nti/app/products/zapier/__init__.py
+++ src/nti/app/products/zapier/__init__.py
@@ -5,6 +5,6 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-import zope
+import zope.i18nmessageid
 
 MessageFactory = zope.i18nmessageid.MessageFactory(__name__)
