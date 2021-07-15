e63d5040538b1de03f074677901f1dcf48b8d537
Bobby Hagen <hagenrd@users.noreply.github.com>
2021-07-14 22:51:12 -0500

    Documentation updates. (#15)

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
     'repoze.sphinx.autointerface',
+    'sphinx.ext.autosectionlabel',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
